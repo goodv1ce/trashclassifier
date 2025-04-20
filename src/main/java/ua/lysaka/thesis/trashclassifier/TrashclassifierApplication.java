0085e1c878870f8b4c7656dc24a23694bcd1a390
goodv1ce <andriy.lysak.2003@gmail.com>
2025-04-20 12:01:09 +0300

    [INIT] init commit

---
+++ src/main/java/ua/lysaka/thesis/trashclassifier/TrashclassifierApplication.java
@@ -0,0 +1,13 @@
+package ua.lysaka.thesis.trashclassifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashclassifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrashclassifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/lysaka/thesis/trashclassifier/TrashclassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lysaka.thesis.trashclassifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashclassifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
